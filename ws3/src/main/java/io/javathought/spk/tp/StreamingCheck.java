7bba02dd61aca9ebe985efe90001688cc27d58d8
javathought <banks@free.fr>
2017-06-14 15:01:34 +0200

    wip(tp3): SparkStreaming - write to redis
    the bad way ? opn/close redis conn

--- ws3/src/main/java/io/javathought/spk/tp/StreamingCheck.java
+++ ws3/src/main/java/io/javathought/spk/tp/StreamingCheck.java
@@ -3,11 +3,14 @@ package io.javathought.spk.tp;
 import org.apache.spark.SparkConf;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.flume.FlumeUtils;
 import org.apache.spark.streaming.flume.SparkFlumeEvent;
 import redis.clients.jedis.Jedis;
+import scala.Serializable;
+import scala.Tuple2;
 
 import java.util.Arrays;
 
@@ -16,6 +19,8 @@ import java.util.Arrays;
  *
  */
 public class StreamingCheck {
+
+    public static final String LOCALHOST = "localhost";
 
     public static void main(String[] args) {
         System.out.print("Flume streaming test");
@@ -28,24 +33,44 @@ public class StreamingCheck {
 //                FlumeUtils.createPollingStream(ssc, "localhost", 4444);
         // pollingStream generate Connection refused exception
 
-        Jedis jedis = new Jedis("localhost");
-
-
         JavaDStream<String> datas = flumeStream.map(e -> new String(e.event().getBody().array(), java.nio.charset.StandardCharsets.UTF_8));
 
         JavaDStream<String> words = datas
-                .flatMap(w -> Arrays.asList(w.split(" ")).iterator());
-
+                .flatMap(w -> Arrays.asList(w.split(" ")).iterator())
+                .map(String::toLowerCase);
 
         words.cache();
+
+        JavaPairDStream<String, Integer> wc = words
+                .mapToPair(s -> new Tuple2<String, Integer>(s, 1))
+                .reduceByKey((b, a) -> a + b);
+
+
         words.print();
         words.count().print();
 
-                ssc.start();
+//        jedis.incrBy("runned tasks", 1);
+
+        wc.foreachRDD(rdd -> {
+            if (!rdd.isEmpty()) rdd
+                    .foreach(p -> {        Jedis jedis = new Jedis(LOCALHOST);
+                        jedis.incrBy(p._1, p._2);
+                        jedis.close();
+                    });
+        });
+
+        ssc.start();
         try {
             ssc.awaitTermination();
         } catch (InterruptedException e) {}
 
 
     }
+
+    private class Incr implements Serializable {
+        public void apply(Jedis jedis, Tuple2<String, Integer> p)  {
+            jedis.incrBy(p._1, p._2);
+        }
+    }
+
 }
